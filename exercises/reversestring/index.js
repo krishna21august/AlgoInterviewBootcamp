e7698f3e1883afab910ee9204582f06b41104c14
krishna21august <krishnakant.singh.min12@gmail.com>
2019-03-23 12:36:36 +0530

    reversing a string with 3possible solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,30 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str
+    .split("")
+    .reduce((reversed, character) => character + reversed, "");
+}
 
 module.exports = reverse;
+
+//***Alternative 1****///
+// function reverse(str) {
+//   //   const arr = str.split("");
+//   //   arr.reverse();
+//   //   return arr.join("");
+//   return str
+//     .split("")
+//     .reverse()
+//     .join("");
+// }
+
+//*****Alternative 2******///
+// function reverse(str) {
+//   let reverse = "";
+//   for (let character of str) {
+//     reverse = character + reverse;
+//   }
+//   return reverse;
+// }
