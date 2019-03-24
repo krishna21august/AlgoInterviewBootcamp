f49af309257fb068e7c024aa180b11f7fe7c5dfa
safe <krishna.s@lucideustech.com>
2019-03-24 13:49:00 +0530

    alternative 2 for anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,34 +8,48 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  const charMapA = buildCharMap(stringA);
-  const charMapB = buildCharMap(stringB);
-
-  /** check if both  objects have same number of keys */
-  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-    return false;
+const anagrams = (strA, strB) => {
+  if (cleanString(strA) === cleanString(strB)) {
+    return true;
   }
-  /** comparing each key of map */
-  for (let char in charMapA) {
-    if (charMapA[char] !== charMapB[char]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const buildCharMap = str => {
-  charMap = {};
-
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    if (!charMap[char]) {
-      charMap[char] = 1;
-    } else {
-      charMap[char]++;
-    }
-  }
-  return charMap;
 };
 
+const cleanString = str => {
+  return str
+    .replace(/[^\w]/g, "")
+    .toLowerCase()
+    .split("")
+    .sort()
+    .join();
+};
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildCharMap(stringA);
+//   const charMapB = buildCharMap(stringB);
+
+//   /** check if both  objects have same number of keys */
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+//   /** comparing each key of map */
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// const buildCharMap = str => {
+//   charMap = {};
+
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     if (!charMap[char]) {
+//       charMap[char] = 1;
+//     } else {
+//       charMap[char]++;
+//     }
+//   }
+//   return charMap;
+// };
