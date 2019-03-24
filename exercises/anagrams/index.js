01b255b9236f62edaf329711791b311eeb421325
krishna21august <krishnakant.singh.min12@gmail.com>
2019-03-24 13:17:22 +0530

    anagrams alternative 1

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,34 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const charMapA = buildCharMap(stringA);
+  const charMapB = buildCharMap(stringB);
+
+  /** check if both  objects have same number of keys */
+  if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+    return false;
+  }
+  /** comparing each key of map */
+  for (let char in charMapA) {
+    if (charMapA[char] !== charMapB[char]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const buildCharMap = str => {
+  charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+  }
+  return charMap;
+};
 
 module.exports = anagrams;
