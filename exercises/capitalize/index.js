e273f575b594a469cb23d068d4d5971712926e6c
krishna21august <krishnakant.singh.min12@gmail.com>
2019-03-24 16:38:35 +0530

    capitalize sgtring alternative 2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,15 +6,28 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-
+//we can easily traverse over a strin .first making first character to capitalize and then checking while traversing that the left character i.e str[i-1] is space
 function capitalize(str) {
-  const words = [];
+  let results = str[0].toUpperCase();
 
-  for (let word of str.split(" ")) {
-    words.push(word[0].toUpperCase() + word.slice(1));
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      results += str[i].toUpperCase();
+    } else {
+      results += str[i];
+    }
   }
-
-  return words.join(" ");
+  return results;
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     const words = [];
+
+//     for (let word of str.split(" ")) {
+//         words.push(word[0].toUpperCase() + word.slice(1));
+//     }
+
+//     return words.join(" ");
+// }
