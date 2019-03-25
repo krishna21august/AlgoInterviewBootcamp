a64fa3d3e64b470b9dff4e46f108158066652e63
krishna21august <krishnakant.singh.min12@gmail.com>
2019-03-25 10:24:11 +0530

    make steps using recursion

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,19 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let column = 0; column < n; column++) {
-      if (column > row) {
-        stair += " ";
-      } else {
-        stair += "#";
-      }
-    }
-
-    console.log(stair);
+function steps(n, row = 0, stair = "") {
+  /*Eliminate code when row becomes equal to n   */
+  if (n === row) {
+    return;
   }
+
+  /*Condition to recursively call same function.For us it is called when sting length of a row equals n .*/
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+
+  steps(n, row, stair);
 }
 
 module.exports = steps;
+
+//
+// function steps(n) {
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let column = 0; column < n; column++) {
+//       if (column > row) {
+//         stair += " ";
+//       } else {
+//         stair += "#";
+//       }
+//     }
+
+//     console.log(stair);
+//   }
+// }
