413aa5052d81b90c5ad977f5c7d465b4951c73f0
safe <krishna.s@lucideustech.com>
2019-03-27 06:50:50 +0530

    fibonacci using recursion

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,15 +9,23 @@
 //   fib(4) === 3
 
 function fib(n) {
-  /*The trick is , we know first two numbers*/
-  const result = [0, 1];
-
-  for (i = 2; i <= n; i++) {
-    const a = result[i - 1];
-    const b = result[i - 2];
-    result.push(a + b);
+  if (n < 2) {
+    return n;
   }
-  return result[n];
+
+  return fib(n - 1) + fib(n - 2);
 }
 
 module.exports = fib;
+
+// function fib(n) {
+//     /*The trick is , we know first two numbers*/
+//     const result = [0, 1];
+
+//     for (i = 2; i <= n; i++) {
+//         const a = result[i - 1];
+//         const b = result[i - 2];
+//         result.push(a + b);
+//     }
+//     return result[n];
+// }
