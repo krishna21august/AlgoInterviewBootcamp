2bcc640da01cce44ea765d121c258d5c4fc221cf
krishna21august <krishnakant.singh.min12@gmail.com>
2019-03-27 13:31:13 +0530

    memoisation for recursive febonnaci series

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,7 +8,21 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
@@ -16,16 +30,19 @@ function fib(n) {
   return fib(n - 1) + fib(n - 2);
 }
 
+const fib = memoize(slowFib);
+
 module.exports = fib;
 
 // function fib(n) {
-//     /*The trick is , we know first two numbers*/
-//     const result = [0, 1];
-
-//     for (i = 2; i <= n; i++) {
-//         const a = result[i - 1];
-//         const b = result[i - 2];
-//         result.push(a + b);
-//     }
-//     return result[n];
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
 // }
