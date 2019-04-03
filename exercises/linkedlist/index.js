6501eedb0421db06d5f3c8717fa5746c1d9f14c3
safe <krishnakant.singh.min12@gmail.com>
2019-04-03 21:53:10 +0530

    get last node

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -29,6 +29,23 @@ class LinkedList {
 
     return counter;
   }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (node.next === null) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
 }
 
 module.exports = { Node, LinkedList };
